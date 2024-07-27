21ec43fe99d6dd25a9ab31bbbee60d5de4c5b919
KING <100894488+tushar8454@users.noreply.github.com>
2024-07-27 19:41:46 +0530

    Update settings.py

--- studybud/settings.py
+++ studybud/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-^b#@hmbgeev6f54_u7sv0yhukn=kvw8=q@cq0y=yg$0di5d@dh
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
